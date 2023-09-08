a1e33394d6e6b54c70bc93cb714edac0befc3dbc
shravanngoswamii <shravanngoswamii@gmail.com>
2023-09-08 19:20:07 +0530

    Recursion

--- Strivers A2Z DSA Course/L6-L10--5 Recursion Lectures/recursion6.cpp
+++ Strivers A2Z DSA Course/L6-L10--5 Recursion Lectures/recursion6.cpp
@@ -1,24 +1,24 @@
-// // Sum of 1st N number
+// Sum of 1st N number
 
-// #include <bits/stdc++.h>
-// using namespace std;
+#include <bits/stdc++.h>
+using namespace std;
 
-// //Parameterised Recursionn
+//Parameterised Recursionn
 
-// void func(int i, int sum){
-// 	if(i<1){
-// 		cout << sum << endl;
-// 		return;
-// 	}
-// 	func(i-1, sum+i);
-// }
+void func(int i, int sum){
+	if(i<1){
+		cout << sum << endl;
+		return;
+	}
+	func(i-1, sum+i);
+}
 
-// int main(){
-// 	int n;
-// 	cin >> n;
-// 	func(n,0);
-// 	return 0;
-// }
+int main(){
+	int n;
+	cin >> n;
+	func(n,0);
+	return 0;
+}
 
 
 // // Functional Recursion
