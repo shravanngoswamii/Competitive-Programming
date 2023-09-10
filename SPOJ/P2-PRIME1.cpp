acf86b951453fafc144662896aa3571be92ef12a
shravanngoswamii <shravanngoswamii@gmail.com>
2023-09-10 18:31:26 +0530

    SPOJ ID 1

---
+++ SPOJ/P2-PRIME1.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int t;
+	cin >> t;
+	for(int i=1; i<=t;i++){
+		int n,m,f=0;
+		cin >> m >> n;
+		for(int j=m; j<=n; j++){
+			f=0;
+			for (int k=2; k<=sqrt(j); k++){
+				if(j%k==0){
+					f=1;
+					break;
+				}
+			}
+			if(f==0 && j!=1){
+				cout << j << endl;
+			}				
+		}
+		cout << endl;
+	}
+}
