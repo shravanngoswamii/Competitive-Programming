4356e8ee1e1d2c396d0cfb8afbbada09097f2063
shravanngoswamii <shravanngoswamii@gmail.com>
2023-09-10 18:31:04 +0530

    SPOJ ID 1

---
+++ SPOJ/P1-TEST.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	for(int i=1; i<100; i++){
+		int n;
+		cin >> n;
+		if(n==42) {
+			break;
+		}
+		cout << n << endl;
+	}
+}
+
+/*
+Input:
+1
+2
+88
+42
+99
+
+Output:
+1
+2
+88
+
+link - https://www.spoj.com/problems/TEST/
+*/
