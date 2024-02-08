6edfb264af35dd0f8d0c514a8c92c35c74c30b98
Shravan Goswami <shravanngoswamii@gmail.com>
2024-02-08 18:29:57 +0530

    ACPC Training Sheet

---
+++ Online Judges/Codeforces/4. ICPC Assiut University - Newcomers Training/Sheet #1 (Data type - Conditions)/X.cpp
@@ -0,0 +1,18 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/X
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	long long l1, l2, r1, r2;
+	cin >> l1 >> r1 >> l2 >> r2;
+	if(r1<l2){
+		cout << -1;
+	}
+	else if(r1>l2 && r1<r2){
+		cout << l2 << " " << r1;
+	}
+	else if(){
+		
+	}
+}
---
+++ Online Judges/Codeforces/4. ICPC Assiut University - Newcomers Training/Sheet #1 (Data type - Conditions)/Y.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    long long m = ((a % 100) * (b % 100) * (c % 100) * (d % 100)) % 100;
+
+    if (m < 10)
+        cout << "0";
+    cout << m << endl;
+
+    return 0;
+}
