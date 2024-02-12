8d6441e161f556800d1fa45eab0e16d02c83405b
Shravan Goswami <shravanngoswamii@gmail.com>
2024-02-12 15:25:48 +0530

    ICPC Assiut Traning Sheet

---
+++ Online Judges/Codeforces/4. ICPC Assiut University - Newcomers Training/Sheet #2 (Loops)/D.cpp
@@ -0,0 +1,18 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/D
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	while(true){
+		int p;
+		cin >> p;
+		if(p==1999){
+			cout << "Correct";
+			break;
+		}
+		else{
+			cout << "Wrong" << endl;
+		}
+	}
+}
